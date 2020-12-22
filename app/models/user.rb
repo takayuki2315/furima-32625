bf8950ae9eb2f296961ea02b7a4551a0ebbcef93
takayuki2315 <t.s151515@icloud.com>
2020-12-22 12:12:31 +0900

    画面遷移のパス設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,13 @@ class User < ApplicationRecord
         :recoverable, :rememberable, :validatable
 
         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-        NAME_REGX =  /\A[ぁ-んァ-ヶ一-龥々]+\z/.freeze
+        NAME_REGEX =  /\A[ぁ-んァ-ヶ一-龥々]+\z/.freeze
         NAME_KANA_REGEX = /\A[ァ-ン]+\z/.freeze
 
         with_options presence: true do
-          validates :password, format: {with: PASSWORD_REGEX, messages: "Password Include both letters and numbers"},
-          validates :first_name, format: {with: NAME_REGX, message: "First name Full-width characters"}
-          validates :last_name, format: {with: NAME_REGX, message: "Last name Full-width characters"}
+          validates_format_of :password, with: PASSWORD_REGEX, messages: "Password Include both letters and numbers"
+          validates :first_name, format: {with: NAME_REGEX, message: "First name Full-width characters"}
+          validates :last_name, format: {with: NAME_REGEX, message: "Last name Full-width characters"}
           validates :first_name_kana, format: {with: NAME_KANA_REGEX, message: "First name kana Full-width katakana characters"}
           validates :last_name_kana, format: {with: NAME_KANA_REGEX, message: "Last name kana Full-width katakana characters"}
           validates :birth_date
