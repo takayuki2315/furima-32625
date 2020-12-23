e3a31f454e437942c08ef562c6871ac35622755a
takayuki2315 <t.s151515@icloud.com>
2020-12-23 23:45:38 +0900

    itemモデル作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,21 @@
 class ItemsController < ApplicationController
   def index
+    
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    
+  end
+
+
+  private
+
+  def item_params
+    
+  end
+
 end
