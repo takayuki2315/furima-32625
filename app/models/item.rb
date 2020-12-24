a4cbed7327c329cab03fa8c8c3999a2077f6cc2c
takayuki2315 <t.s151515@icloud.com>
2020-12-25 03:57:02 +0900

    販売価格をJavaScriptで実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,11 +12,14 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :text
-    validates :price
-    validates :category_id, numericality: { other_than: 1 }
-    validates :condition_id, numericality: { other_than: 1 }
-    validates :delivery_fee_id, numericality: { other_than: 1 }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range" }
+    validates :category_id, numericality: { other_than: 0 }
+    validates :condition_id, numericality: { other_than: 0 }
+    validates :delivery_fee_id, numericality: { other_than: 0 }
     validates :prefecture_id, numericality: { other_than: 0 }
-    validates :days_to_delivery_id, numericality: { other_than: 1 }
+    validates :days_to_delivery_id, numericality: { other_than: 0 }
   end
+
+  PRICE_REGEX = /\A[0-9]+\z/.freeze
+  validates :price, format: { with: PRICE_REGEX }
 end
