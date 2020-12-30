45315aea588d743c33b4d81ad2d2abefaab4878f
takayuki2315 <t.s151515@icloud.com>
2020-12-30 11:44:08 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -56,7 +56,9 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to root_path unless current_user == @item.user
+    if (current_user != @item.user) || @item.order.present?
+      redirect_to root_path 
+    end
   end
 
 end
