70d33eee4d2aa33e71d80147982615691a22e81e
takayuki2315 <t.s151515@icloud.com>
2020-12-31 21:41:34 +0900

    AWS S3

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々]+\z/.freeze
@@ -10,7 +10,7 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates_format_of :password, with: PASSWORD_REGEX, messages: 'Password Include both letters and numbers',
-                                   length: { minimum: 6 }
+                                  length: { minimum: 6 }
     validates :first_name, format: { with: NAME_REGEX, message: 'First name Full-width characters' }
     validates :last_name, format: { with: NAME_REGEX, message: 'Last name Full-width characters' }
     validates :first_name_kana, format: { with: NAME_KANA_REGEX, message: 'First name kana Full-width katakana characters' }
