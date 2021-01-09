691fd3c15cc06601935e04b1257f86ce3c18b16f
takayuki2315 <t.s151515@icloud.com>
2021-01-09 21:05:13 +0900

    capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.15.0'
 set :application, 'furima-32625'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:takayuki2315/furima32625.git'
+set :repo_url,  'git@github.com:takayuki2315/furima-32625.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
